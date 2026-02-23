8b597f4b5183194858ff7fa68a6429a6b7ba08e6
AdvancedHermit <christiannkya@outlook.com>
2026-02-23 14:20:13 +0100

    Uses label

---
+++ src/components/preprocessors/BasicPreprocessorWordCleaner.cpp
@@ -0,0 +1,67 @@
+#include "../../core/interfaces.h"
+
+#include <fstream>
+
+class BasicPreprocessorWordCleaner : public IPreprocessor {
+public:
+    ~BasicPreprocessorWordCleaner() override = default;
+    void* preprocess(std::string filename, IStore* store) override;
+
+};
+
+
+void* BasicPreprocessorWordCleaner::preprocess(std::string filename, IStore *store) {
+    std::ifstream file;
+    file.open (filename);
+    if (!file.good() || !file.is_open()) {
+        printf("Error reading file");
+        return nullptr;
+    }
+
+    Doc document{};
+    std::string word;
+    std::string line;
+
+    short take_next = 1;
+    long long before_first_word = file.tellg();
+
+    int dbcounter = 0;
+    while (std::getline(file, line)) {
+        std::vector<std::string> words;
+        getWordsFromLine(line, patterns, words);
+
+        for (int i = 0;i < words.size(); ++i) {
+            word = words[i];
+            if (take_next) {
+                std::string full_line = word;
+                for (int wc = 1; wc < words.size(); ++wc) full_line += " " + words[wc];
+                document.title = full_line;
+                char last_char_title = document.title.back();
+                if (last_char_title == ',' || last_char_title == '.' || last_char_title == '?') {
+                    document.title.erase(document.title.size() - 1);
+                }
+                document.start_loc = before_first_word;
+                store->add_document(document);
+            }
+            char last_char = word.back();
+            if (last_char == ',' || last_char == '.' || last_char == '?') {
+                word.erase(word.size() - 1);
+            }
+
+            std::vector<std::string> clean_words;
+            cleanString(word, clean_words);
+            for (const std::string& clean_word : clean_words) {
+                store->add(remove_unwanted_and_trim(&clean_word), document);
+            }
+
+            take_next = word == END;
+        }
+        before_first_word = file.tellg();
+
+    }
+    std::cout << dbcounter << std::endl;
+    file.close();
+    return nullptr;
+}
+
+
