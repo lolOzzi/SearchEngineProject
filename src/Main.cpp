29522dba2016dc52bbc8fa5291a4656a1196df66
Oliver5811 <88721999+Oliver5811@users.noreply.github.com>
2026-03-08 19:49:16 +0100

    bool parser

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,7 @@
 #include "components/hashers/SimpleFingerprint.h"
 #include "components/searchers/BasicSearcher.cpp"
 #include "components/stores/GenericFKSWithDocId.cpp"
-//#include "components/stores/DynamicFKSRadixTree.cpp"
+#include "components/stores/BasicHashTable.cpp"
 #include "components/searchers/BooleanSearcher.cpp"
 #include "components/stores/FuzzyDynamicFKSRadixTree.cpp"
 //#include "components/stores/TreeWrapper.cpp"
@@ -26,31 +26,33 @@ int main(int argc, char* argv[]) {
 
     BasicPreprocessorWordCleaner preprocessor;
     SimpleFingerprint hasher;
-    BasicSearcher searcher;
-    GenericFKSWithDocId store = GenericFKSWithDocId(3'000'000, &hasher);
+    //BasicSearcher searcher;
+    BooleanSearcher searcher;
+
+    BasicHashTable store = BasicHashTable(3'000'000, &hasher);
 
     Index index = Index(&store, &preprocessor, &hasher, &searcher, nullptr, nullptr);
     // 57408850400
     // 348283157567 ns, fuzzy
     printf("Started preprocessing \n");
-    auto t0 = chrono::steady_clock::now();
+   // auto t0 = chrono::steady_clock::now();
     std::string filename = "data/WestburyLab.wikicorp.201004_100KB.txt";
     index.preprocess(filename);
-    auto t1 = chrono::steady_clock::now();
-    auto elapsed = duration_cast<chrono::nanoseconds>(t1 - t0).count();
-    std::cout << " elapsed=" << elapsed << " ns\n";
+  //  auto t1 = chrono::steady_clock::now();
+   // auto elapsed = duration_cast<chrono::nanoseconds>(t1 - t0).count();
+  //  std::cout << " elapsed=" << elapsed << " ns\n";
     printf("Finished preprocessing \n");
 
     printf("Start searching \n");
     SearchQuery q;
-    q.q = "albedo";
+    q.q = "(albedo & is) | is & hike ";
     std::vector<Doc> res = index.search(q);
     printf("Finished searching \n");
     for (std::vector<Doc>::iterator it = res.begin(); it != res.end(); ++it) {
         std::cout << it->title << " "  << endl;
     }
 
-    test(&index);
+    //test(&index);
 
     return 0;
 }
